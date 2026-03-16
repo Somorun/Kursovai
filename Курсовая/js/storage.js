6add9198ff4e40638db6475e654f6ef2d54f40e4
Somorun <ruslan60.06@mail.ru>
2026-03-16 18:01:44 +0600

    Delete Курсовая directory

--- Курсовая/js/storage.js
+++
@@ -1,8 +0,0 @@
-function getData() {
-    const data = localStorage.getItem("expenses");
-    return data ? JSON.parse(data) : [];
-}
-
-function saveData(data) {
-    localStorage.setItem("expenses", JSON.stringify(data));
-}
